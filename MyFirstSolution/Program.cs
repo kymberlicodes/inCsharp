f5118d3f6184577233adb11b61dda3dc939f248a
Kymberli <kymberlicodes@gmail.com>
2018-11-10 10:03:05 -0600

    First commit.

---
+++ MyFirstSolution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyFirstSolution
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+
+            int var1 = 1;
+            int var2 = 2;
+            Console.WriteLine(var1 + var2);
+
+            string var3 = "3";
+            string var4 = "4";
+            Console.WriteLine(var3 + var4);
+        }
+    }
+}
